5407c58ae1d00eb9fe299728587a87064c7c9907
shalan000 <muhammedshaalan1011@gmail.com>
2023-04-25 18:06:15 +0530

    qq

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 3, 25, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 3, 26, 8)}); // year, month, date, hour
         });
 });		
 
